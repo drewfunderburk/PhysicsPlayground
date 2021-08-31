0bbda69e31240ba84484309fbbed9e3597f9f5b5
Drew <kevinfunderburkbusiness@gmail.com>
2021-08-31 13:01:51 -0500

    Character Controller

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _baseSpeed = 1;
+    [SerializeField] private float _gravityMultiplier = 1;
+    [SerializeField] private float _jumpForce = 100;
+
+    private CharacterController _controller;
+
+    private Vector3 _desiredGroundVelocity;
+    private Vector3 _desiredAirVelocity;
+    private bool _isJumpDesired;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        // Get movement input
+        _desiredGroundVelocity.x = (Input.GetAxisRaw("Horizontal") * transform.right).x;
+        _desiredGroundVelocity.y = 0;
+        _desiredGroundVelocity.z = (Input.GetAxisRaw("Vertical") * transform.forward).z;
+
+        // Scale movement to desired velocity
+        _desiredGroundVelocity.Normalize();
+        _desiredGroundVelocity *= _baseSpeed;
+
+        // Get jump input
+        _isJumpDesired = Input.GetButtonDown("Jump");
+
+        if (_isJumpDesired)
+        {
+            _desiredAirVelocity.y = _jumpForce;
+            _isJumpDesired = false;
+        }
+
+        // Gravity
+        _desiredAirVelocity += Physics.gravity * _gravityMultiplier * Time.deltaTime;
+
+        // Move
+        _controller.Move((_desiredGroundVelocity + _desiredAirVelocity) * Time.deltaTime);
+    }
+}
