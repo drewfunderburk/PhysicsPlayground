2ff2de18568b1a9dc4af4e26cbe244b55adb703b
Drew <kevinfunderburkbusiness@gmail.com>
2021-08-31 15:51:52 -0500

    Made character controls relative to camera

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -6,43 +6,64 @@ public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private float _baseSpeed = 1;
     [SerializeField] private float _gravityMultiplier = 1;
-    [SerializeField] private float _jumpForce = 100;
+    [SerializeField] private float _jumpHeight = 1;
+    [SerializeField] [Range(0, 1)] private float _airControl = 1;
 
     private CharacterController _controller;
+    private Transform _camTransform;
 
-    private Vector3 _desiredGroundVelocity;
+    private Vector3 _desiredVelocity;
     private Vector3 _desiredAirVelocity;
     private bool _isJumpDesired;
 
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
+        _camTransform = Camera.main.gameObject.transform;
     }
 
     private void Update()
     {
         // Get movement input
-        _desiredGroundVelocity.x = (Input.GetAxisRaw("Horizontal") * transform.right).x;
-        _desiredGroundVelocity.y = 0;
-        _desiredGroundVelocity.z = (Input.GetAxisRaw("Vertical") * transform.forward).z;
+        _desiredVelocity.x = Input.GetAxisRaw("Horizontal");
+        _desiredVelocity.y = 0;
+        _desiredVelocity.z = Input.GetAxisRaw("Vertical");
+
+        // Get camera forward
+        Vector3 cameraForward = _camTransform.forward;
+        cameraForward.y = 0;
+        cameraForward.Normalize();
+
+        // Get camera right
+        Vector3 cameraRight = _camTransform.right;
+
+        _desiredVelocity = (_desiredVelocity.x * cameraRight + _desiredVelocity.z * cameraForward);
 
         // Scale movement to desired velocity
-        _desiredGroundVelocity.Normalize();
-        _desiredGroundVelocity *= _baseSpeed;
+        _desiredVelocity.Normalize();
+        _desiredVelocity *= _baseSpeed;
+
+        // Apply air control
 
         // Get jump input
-        _isJumpDesired = Input.GetButtonDown("Jump");
+        _isJumpDesired = Input.GetButton("Jump");
 
-        if (_isJumpDesired)
+        if (_isJumpDesired && _controller.isGrounded)
         {
-            _desiredAirVelocity.y = _jumpForce;
-            _isJumpDesired = false;
+            _desiredAirVelocity = transform.up * Mathf.Sqrt(_jumpHeight * -2f * (Physics.gravity.y * _gravityMultiplier));
+            //_isJumpDesired = false;
         }
 
+        if (!_isJumpDesired && _controller.isGrounded)
+        {
+            _desiredAirVelocity.y = 0;
+        }
+
+
         // Gravity
-        _desiredAirVelocity += Physics.gravity * _gravityMultiplier * Time.deltaTime;
+        _desiredAirVelocity += Physics.gravity * _gravityMultiplier * Time.fixedDeltaTime;
 
         // Move
-        _controller.Move((_desiredGroundVelocity + _desiredAirVelocity) * Time.deltaTime);
+        _controller.Move((_desiredVelocity + _desiredAirVelocity) * Time.fixedDeltaTime);
     }
 }
