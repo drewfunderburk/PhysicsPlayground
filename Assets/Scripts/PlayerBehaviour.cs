d7615e4aa4987a8f3a01d4d44761c68ee058f7ec
Drew <kevinfunderburkbusiness@gmail.com>
2021-09-09 10:14:52 -0500

    Updated playerbehaviour to add force on collision

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -65,22 +65,42 @@ public class PlayerBehaviour : MonoBehaviour
         // Gravity
         _desiredAirVelocity += Physics.gravity * _gravityMultiplier * Time.fixedDeltaTime;
 
+        float animatorSpeed = 0;
+
         // Update animations
         if (_faceWithCamera)
         {
             transform.forward = cameraForward;
-            _animator.SetFloat("Speed", inputForward);
+            animatorSpeed = inputForward;
             _animator.SetFloat("Direction", inputRight);
         }
         else if (_desiredVelocity != Vector3.zero)
         {
             transform.forward = _desiredVelocity.normalized;
-            _animator.SetFloat("Speed", _desiredVelocity.magnitude / _baseSpeed);
+            animatorSpeed = _desiredVelocity.magnitude / _baseSpeed;
         }
+
+        _animator.SetFloat("Speed", animatorSpeed);
 
         _animator.SetBool("Jump", !_controller.isGrounded);
 
         // Move
         _controller.Move((_desiredVelocity + _desiredAirVelocity) * Time.fixedDeltaTime);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("Finish"))
+        {
+            _controller.enabled = false;
+            _animator.enabled = false;
+
+            Rigidbody[] rbs = GetComponentsInChildren<Rigidbody>();
+
+            foreach (Rigidbody rb in rbs)
+                rb.AddExplosionForce(500, hit.point, 50);
+
+            this.enabled = false;
+        }
     }
 }
