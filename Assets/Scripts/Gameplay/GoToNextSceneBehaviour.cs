67e6cc9e12b4c5ed365f1a10b8008a9d2a2e4648
Drew <kevinfunderburkbusiness@gmail.com>
2021-09-23 16:50:21 -0500

    Added simple Start scene

---
+++ Assets/Scripts/Gameplay/GoToNextSceneBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNextSceneBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _delay = 0;
+
+    public void GoToNextScene()
+    {
+        if (_delay > 0)
+            StartCoroutine(GoToNextSceneCoroutine());
+        else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private IEnumerator GoToNextSceneCoroutine()
+    {
+        yield return new WaitForSeconds(_delay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
